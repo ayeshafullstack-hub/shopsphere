57aee66a0c4d0a309130609c08a88e341d9f7254
Jamman Ayesha <jammanayesha02@gmail.com>
2026-08-31 04:13:15 +0530

    Implement Product Management API

---
+++ backend/products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "stock", "created_at")
+    list_display_links = ("name",)
+    search_fields = ("name", "description")
+    list_filter = ("category",)
+    ordering = ("name",)
