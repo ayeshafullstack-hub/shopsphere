a2521c5a574b2c103e91caa54bd92a7d756bee8f
Jamman Ayesha <jammanayesha02@gmail.com>
2026-09-03 21:34:01 +0530

    Adding missing data

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,3 +137,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "config.exceptions.custom_exception_handler",
+
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+
+    "PAGE_SIZE": 5,
+}
