3c481a60e1f8088c42dd24b54cd6d23421e0d0c9
Jamman Ayesha <jammanayesha02@gmail.com>
2026-09-06 16:23:37 +0530

    Add category filtering

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,11 +8,21 @@ from .serializers import ProductSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    ## queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     filter_backends = [filters.SearchFilter]
     search_fields = ["name","description"]
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        
+        category = self.request.query_params.get("category")
+        
+        if category:
+            queryset = queryset.filter(category = category)
+        
+        return queryset
 
 
 class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
