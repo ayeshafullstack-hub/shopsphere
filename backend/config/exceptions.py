de217317f2e118f66ded6d6416e4d456cc435743
Jamman Ayesha <jammanayesha02@gmail.com>
2026-09-08 09:43:55 +0530

    Correcting mistakes in exceptions.py

--- backend/config/exceptions.py
+++ backend/config/exceptions.py
@@ -2,7 +2,7 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc,context):
     response = exception_handler(exc,context)
-    if response is not None and response.status__code == 404:
+    if response is not None and response.status_code == 404:
         response.data = {
             "error" : "Product not found"
         }
