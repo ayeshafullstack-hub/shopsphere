160b62739c95b213fa2713402933df94d0f07fef
Jamman Ayesha <jammanayesha02@gmail.com>
2026-09-15 03:11:41 +0530

    Add refresh token path in config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +31,10 @@ urlpatterns = [
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
     ),
 ]
